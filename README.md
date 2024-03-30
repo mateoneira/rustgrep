a61a2b0e686064b97bafcf68572ba852854bf65e
Mateo Neira <mateo0719@gmail.com>
2024-03-30 22:34:43 +0000

    added env variables

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple command line program for finding text within a file. 
+
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,12 @@ fn main() {
     let args = env::args().collect::<Vec<String>>();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!("searching for {}", config.query);
-    println!("In file{}", config.file_path);
 
     if let Err(e) = rustgrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
