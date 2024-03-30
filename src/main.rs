a62c4d32b6e05c6728d40e4e96752c22a0f00c1c
Mateo Neira <mateo0719@gmail.com>
2024-03-30 21:43:33 +0000

    initial impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use rustgrep::Config;
+use std::env;
+use std::process;
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    println!("searching for {}", config.query);
+    println!("In file{}", config.file_path);
+
+    if let Err(e) = rustgrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
